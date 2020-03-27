fc01951c27e6744886d7066f9d61b5ddd56c5653
keroro-o <obata19760205akinobu@outlook.jp>
2020-03-27 16:24:09 +0900

    ’メモ化’によるアルゴリズムの改善

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 'use strict';
 
+const memo = new Map();   // Map(連想配列)を作成して、変数に格納
+memo.set(0, 0);           // 0番目と1番目の答えは予め定義されている
+memo.set(1, 1);
+
+/**
+ * 連想配列 Map を利用してフィボナッチ数列を作る関数。
+ * @param {Number} n
+ * @returns {Number}
+ */
 function fib(n) {
-  if (n === 0) {
-    return 0;
-  } else if (n === 1) {
-    return 1;
+  if (memo.has(n)) {      // もし、Map が、n をキーとした答えを持っていれば
+    return memo.get(n);   // その値をそのまま関数の値として返し、
   }
-  return fib(n - 1) + fib(n - 2);   // 再帰関数
+  const value = fib(n - 1) + fib(n - 2);
+  memo.set(n, value);
+  return value;           // そうでない場合、再帰関数を計算して値を求め、それをMapに格納した後に返す。
 }
 
 const length = 40;
